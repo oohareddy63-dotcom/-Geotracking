192bbb2be700e5046916f1998a3a7e8ec87cc36e
oohareddy63-dotcom <oohareddy63@gmail.com>
2026-05-19 22:47:33 +0530

    Fix formatting in geo.js

--- server/utils/geo.js
+++ server/utils/geo.js
@@ -9,7 +9,6 @@ function calculateDistance(lat1, lon1, lat2, lon2) {
             Math.cos(φ1) * Math.cos(φ2) *
             Math.sin(Δλ/2) * Math.sin(Δλ/2);
   const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-
   const distance = R * c; // in meters
   return distance;
 }
